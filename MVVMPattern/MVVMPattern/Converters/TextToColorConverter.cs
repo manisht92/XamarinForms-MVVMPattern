a8d04b50aafda5c59edd4ab2a08de479e00c90b3
Manish Tiwari <manish.t92@gmail.com>
2019-01-23 08:24:16 +0530

    IValueConverter Implemented

---
+++ MVVMPattern/MVVMPattern/Converters/TextToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MVVMPattern.Converters
+{
+    public class TextToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var text = value.ToString();
+                if (text.Contains("UI"))
+                {
+                    return Color.Green;
+                }
+            }
+            return Color.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "";
+        }
+    }
+}
